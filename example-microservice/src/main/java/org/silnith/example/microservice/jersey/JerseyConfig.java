d6227531b1a7fb50a637248a54e7a0aa6ec7ccc7
kent <silnith@gmail.com>
2018-08-26 23:37:07 -0700

    Add OpenAPI (Swagger v3)

--- example-microservice/src/main/java/org/silnith/example/microservice/jersey/JerseyConfig.java
+++ example-microservice/src/main/java/org/silnith/example/microservice/jersey/JerseyConfig.java
@@ -1,14 +1,18 @@
 package org.silnith.example.microservice.jersey;
 
+import javax.ws.rs.ApplicationPath;
+
 import org.glassfish.jersey.server.ResourceConfig;
 import org.silnith.example.microservice.jaxrs.TransactionController;
 import org.springframework.stereotype.Component;
 
 @Component
+@ApplicationPath("/")
 public class JerseyConfig extends ResourceConfig {
 
     public JerseyConfig() {
         super(TransactionController.class);
+        super.packages("io.swagger.sample.resource", "io.swagger.v3.jaxrs2.integration.resources");
     }
 
 }
